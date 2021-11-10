eb253148181098ba38634dc8b78bc5b49243eeee
Larissa Novais <larinovais54@gmail.com>
2021-11-10 11:54:34 -0300

    Modelar class model conta

--- src/main/java/zup/com/br/gerenciador/contas/Conta.java
+++ src/main/java/zup/com/br/gerenciador/contas/Conta.java
@@ -1,5 +1,84 @@
 package zup.com.br.gerenciador.contas;
 
-public class Conta {
+import zup.com.br.gerenciador.contas.enums.Status;
+import zup.com.br.gerenciador.contas.enums.Tipo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contas")
+public class Conta {
+    @Id
+    private int id;
+    @Column(nullable = false)
+    private String nome;
+    private double valor;
+    private LocalDate dataDeVencimento;
+    private LocalDateTime dataDePagamento;
+    private Status status;
+    private Tipo tipo;
+
+    public Conta() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDateTime getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDateTime dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
 }
