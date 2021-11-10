c29fcbd33ef56f5d07fee25b10877773c5135b39
Larissa Novais <larinovais54@gmail.com>
2021-11-10 12:55:49 -0300

    Adicionar anotação de servico

--- src/main/java/zup/com/br/gerenciador/contas/ContaService.java
+++ src/main/java/zup/com/br/gerenciador/contas/ContaService.java
@@ -1,5 +1,12 @@
 package zup.com.br.gerenciador.contas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContaService {
+
+    @Autowired
+    private ContaRepository contaRepository;
 
 }
