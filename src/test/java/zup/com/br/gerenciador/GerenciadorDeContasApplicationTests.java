d047bfddedc8004f52117d3cb2c41855d9c3a03f
Larissa Novais <larinovais54@gmail.com>
2021-11-10 11:23:59 -0300

    Inicio de projeto

---
+++ src/test/java/zup/com/br/gerenciador/GerenciadorDeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.com.br.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
