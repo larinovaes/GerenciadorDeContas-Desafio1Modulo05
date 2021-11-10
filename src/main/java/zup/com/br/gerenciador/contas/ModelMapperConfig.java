57a3ef6be29a1a69ee660fff862070b62bc4eb06
Larissa Novais <larinovais54@gmail.com>
2021-11-10 12:56:50 -0300

    Criar class de configuração para modelmapper

--- src/main/java/zup/com/br/gerenciador/contas/ModelMapperConfig.java
+++ src/main/java/zup/com/br/gerenciador/contas/ModelMapperConfig.java
@@ -1,5 +1,13 @@
 package zup.com.br.gerenciador.contas;
 
-public class ModelMapper {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public org.modelmapper.ModelMapper modelMapper() {
+        return new org.modelmapper.ModelMapper();
+    }
 }
