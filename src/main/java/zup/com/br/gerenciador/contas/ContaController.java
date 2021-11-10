b2514ca45ee9aee918da6612287a52363ce1a203
Larissa Novais <larinovais54@gmail.com>
2021-11-10 11:56:54 -0300

    Modelar class controller

--- src/main/java/zup/com/br/gerenciador/contas/ContaController.java
+++ src/main/java/zup/com/br/gerenciador/contas/ContaController.java
@@ -1,4 +1,14 @@
 package zup.com.br.gerenciador.contas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contas")
 public class ContaController {
+
+    @Autowired
+    private ContaService contaService;
+
 }
