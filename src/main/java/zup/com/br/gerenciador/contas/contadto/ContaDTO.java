dca68f521747c2d7c73cad9e0fe9aa6de3361852
Larissa Novais <larinovais54@gmail.com>
2021-11-10 11:41:47 -0300

    Modelar class DTO

--- src/main/java/zup/com/br/gerenciador/contas/contadto/ContaDTO.java
+++ src/main/java/zup/com/br/gerenciador/contas/contadto/ContaDTO.java
@@ -1,5 +1,69 @@
 package zup.com.br.gerenciador.contas.contadto;
 
+import zup.com.br.gerenciador.contas.enums.Status;
+import zup.com.br.gerenciador.contas.enums.Tipo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class ContaDTO {
 
+    private String nome;
+    private double valor;
+    private LocalDate dataDeVencimento;
+    private LocalDateTime dataDePagamento;
+    private Status status;
+    private Tipo tipo;
+
+    public ContaDTO() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public LocalDateTime getDataDePagamento() {
+        return dataDePagamento;
+    }
+
+    public void setDataDePagamento(LocalDateTime dataDePagamento) {
+        this.dataDePagamento = dataDePagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
 }
