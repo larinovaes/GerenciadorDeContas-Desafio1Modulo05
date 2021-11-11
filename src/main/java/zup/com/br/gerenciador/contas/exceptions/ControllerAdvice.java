4885dc5fa75a9fbbf9b573fa19562c371652c3f5
Larissa Novais <larinovais54@gmail.com>
2021-11-11 13:49:44 -0300

    Criar metodo de erro para campos invalidos

--- src/main/java/zup/com/br/gerenciador/contas/exceptions/ControllerAdvice.java
+++ src/main/java/zup/com/br/gerenciador/contas/exceptions/ControllerAdvice.java
@@ -1,8 +1,16 @@
 package zup.com.br.gerenciador.contas.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    @ExceptionHandler(CamposInvalidos.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public MensagemDeErro erroCampoInvalido(CamposInvalidos camposInvalidos) {
+        return new MensagemDeErro("Campo obrigatorio, por favor digite novamente");
+    }
 }
