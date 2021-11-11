c515834a9e173652e5c1b59623d03fa84a4581c7
Larissa Novais <larinovais54@gmail.com>
2021-11-11 15:50:50 -0300

    Criar class de exception

--- src/main/java/zup/com/br/gerenciador/contas/exceptions/ContaNaoEncontradaException.java
+++ src/main/java/zup/com/br/gerenciador/contas/exceptions/ContaNaoEncontradaException.java
@@ -1,2 +1,15 @@
-package zup.com.br.gerenciador.contas.exceptions;public class ContaNaoEncontradaException {
+package zup.com.br.gerenciador.contas.exceptions;
+
+public class ContaNaoEncontradaException extends RuntimeException{
+
+    private Integer idConta;
+
+
+    public ContaNaoEncontradaException(Integer idConta) {
+        this.idConta = idConta;
+    }
+
+    public Integer getIdConta() {
+        return idConta;
+    }
 }
