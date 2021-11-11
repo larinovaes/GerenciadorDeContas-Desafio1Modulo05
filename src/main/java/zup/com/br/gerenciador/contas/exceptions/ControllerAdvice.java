fec532d3ad991868af0430ef0a6b3e8ab8202753
Larissa Novais <larinovais54@gmail.com>
2021-11-11 12:21:12 -0300

    Criar class controller Advice

---
+++ src/main/java/zup/com/br/gerenciador/contas/exceptions/ControllerAdvice.java
@@ -0,0 +1,8 @@
+package zup.com.br.gerenciador.contas.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+}
