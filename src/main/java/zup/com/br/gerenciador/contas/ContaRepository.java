f360e1570b32a89707a092048c9b09dc4536d9fd
Larissa Novais <larinovais54@gmail.com>
2021-11-12 15:22:58 -0300

    Criar query de valorAproximado no filtro valor

--- src/main/java/zup/com/br/gerenciador/contas/ContaRepository.java
+++ src/main/java/zup/com/br/gerenciador/contas/ContaRepository.java
@@ -20,6 +20,10 @@ public interface ContaRepository extends CrudRepository<Conta, Integer> {
     @Query(value = "SELECT * FROM contas WHERE valor > :valor", nativeQuery = true)
     List<Conta> findAllValorMaiorQue(@Param("valor") double valor);
 
+    @Query(value = "SELECT * FROM contas WHERE valor BETWEEN :valorMenor AND :valorMaior", nativeQuery = true)
+    List<Conta> findAllValorEntre(@Param("valorMenor") double valorMenor, @Param("valorMaior") double valorMaior);
+
+
     List<Conta> findAllBystatus(Status status);
 
     List<Conta> findAllBytipo(Tipo tipo);
