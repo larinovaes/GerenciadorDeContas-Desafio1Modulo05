126040eadce6126e6123121ad302c3a1759eea61
Larissa Novais <larinovais54@gmail.com>
2021-11-11 22:53:04 -0300

    Adicionar filtro na contaRepository

--- src/main/java/zup/com/br/gerenciador/contas/ContaRepository.java
+++ src/main/java/zup/com/br/gerenciador/contas/ContaRepository.java
@@ -2,6 +2,7 @@ package zup.com.br.gerenciador.contas;
 
 import org.springframework.data.repository.CrudRepository;
 import zup.com.br.gerenciador.contas.enums.Status;
+import zup.com.br.gerenciador.contas.enums.Tipo;
 import zup.com.br.gerenciador.contas.model.Conta;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface ContaRepository extends CrudRepository<Conta, Integer> {
     List<Conta> findAllByvalor(double valor);
 
     List<Conta> findAllBystatus(Status status);
+
+    List<Conta> findAllBytipo(Tipo tipo);
 }
