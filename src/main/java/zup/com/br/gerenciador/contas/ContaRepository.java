c2fd61594cce2f11fe4ea96ee6d908b9162664e4
Larissa Novais <larinovais54@gmail.com>
2021-11-12 15:21:01 -0300

    Criar query no filtro valor

--- src/main/java/zup/com/br/gerenciador/contas/ContaRepository.java
+++ src/main/java/zup/com/br/gerenciador/contas/ContaRepository.java
@@ -1,6 +1,8 @@
 package zup.com.br.gerenciador.contas;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import zup.com.br.gerenciador.contas.enums.Status;
 import zup.com.br.gerenciador.contas.enums.Tipo;
 import zup.com.br.gerenciador.contas.model.Conta;
@@ -12,7 +14,8 @@ public interface ContaRepository extends CrudRepository<Conta, Integer> {
 
     List<Conta> findAllBynome(String nome);
 
-    List<Conta> findAllByvalor( double valor);
+    @Query(value = "SELECT * FROM contas WHERE valor < :valor", nativeQuery = true)
+    List<Conta> findAllByvalor(@Param("valor") double valor);
 
     List<Conta> findAllBystatus(Status status);
 
